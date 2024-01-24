ed27c3fc68910bd98e3791d907f08b1f40ba3183
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-24 11:10:58 +0700

    create nested struct validate

--- validation_test.go
+++ validation_test.go
@@ -153,3 +153,36 @@ func TestStructCrossField(t *testing.T) {
 	}
 
 }
+
+func TestNestedStruct(t *testing.T) {
+
+	type Address struct {
+		City    string `validate:"required"`
+		Country string `validate:"required"`
+	}
+
+	type RegisterUser struct {
+		Email           string `validate:"required,email"`
+		Password        string `validate:"required,min=5"`
+		ConfirmPassword string `validate:"required,min=5,eqfield=Password"`
+		Address         Address
+	}
+
+	validate := validator.New()
+
+	registerUser := RegisterUser{
+		Email:           "test@gmail.com",
+		Password:        "test123",
+		ConfirmPassword: "test123",
+		Address: Address{
+			City:    "",
+			Country: "",
+		},
+	}
+
+	err := validate.Struct(registerUser)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+}
