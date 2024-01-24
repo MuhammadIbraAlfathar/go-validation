bc7e5de30795d126f38234bb14ca846b4e7d6705
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-24 10:36:54 +0700

    add multiple tag validate

--- validation_test.go
+++ validation_test.go
@@ -29,7 +29,19 @@ func TestValidateTwoVariable(t *testing.T) {
 	password := "test123"
 	confirmPassword := "test123"
 
+	//membandingkan dua nilai variable harus sama
 	err := validate.VarWithValue(password, confirmPassword, "eqfield")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func TestMultipleTag(t *testing.T) {
+	validate := validator.New()
+	user := "1232312"
+
+	err := validate.Var(user, "required,number")
 
 	if err != nil {
 		fmt.Println(err.Error())
