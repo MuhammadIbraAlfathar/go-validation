e214fe32c26cc59cdd2707b170720c52d1428c79
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-24 10:19:52 +0700

    add validate variable

--- validation_test.go
+++ validation_test.go
@@ -1,6 +1,7 @@
 package go_validation
 
 import (
+	"fmt"
 	"github.com/go-playground/validator/v10"
 	"testing"
 )
@@ -9,5 +10,16 @@ func TestValidate(t *testing.T) {
 	validate := validator.New()
 	if validate == nil {
 		t.Error("Validate is nill")
+	}
+}
+
+func TestValidateVariable(t *testing.T) {
+	validate := validator.New()
+	user := ""
+
+	err := validate.Var(user, "required")
+
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 }
